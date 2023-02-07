5b8b058cf7a4fc8edcd927abaac539abc146b18b
alvinchenn <alvinc101@gmail.com>
2023-02-06 16:15:31 -0800

    Initial Commit

---
+++ cs125project/cs125projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs125projectApp.swift
+//  cs125project
+//
+//  Created by Alvin Chen on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct cs125projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
